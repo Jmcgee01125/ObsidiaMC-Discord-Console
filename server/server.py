2730ee05a472e7ab02eee5b39f8a3c6d3a8514d0
Jmcgee1125 <jmcgee01125@gmail.com>
2022-04-07 00:23:48 -0400

    cross platform server fix

--- server/server.py
+++ server/server.py
@@ -42,8 +42,11 @@ class ServerRunner:
     async def start(self):
         '''Start the server process if not already started.'''
         if (self._server == None or not self.is_active()):
-            self._server = subprocess.Popen(["java"] + self._args + ["-jar"] + [self._jarname] + ["-nogui"],
-                                            stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True, cwd=self.server_directory)
+            cmd = f"java "
+            for arg in self._args:
+                cmd += f"{arg} "
+            cmd += f"-jar {self._jarname} -nogui"
+            self._server = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True, cwd=self.server_directory)
             threading.Thread(target=self._start_async_log_listener, name="ServerLogListener", daemon=True).start()
 
     def _start_async_log_listener(self):
