bc0189e16648f0b75b4d970fe39950cc4f9b1ec3
Jmcgee1125 <jmcgee01125@gmail.com>
2022-04-09 22:18:59 -0400

    log info like crashes to obsidia log file

--- __main__.py
+++ __main__.py
@@ -3,14 +3,17 @@ from config.configs import ObsidiaConfigParser
 import bot.discord_server as discord_server
 from server.server import ServerListener
 import threading
+import time
 import os
 
 
 class ConsolePrintListener:
-    def __init__(self, manager: ServerManager):
+    def __init__(self, manager: ServerManager, logging_directory: str):
         self._manager: ServerManager = manager
         self._listener: ServerListener = ServerListener(manager.server)
         self._should_shut_down: bool = False
+        self._logfile: str = os.path.join(logging_directory, f"{int(time.time())}.log")
+        os.makedirs(logging_directory, exist_ok=True)
 
     def start(self):
         threading.Thread(target=self._print_queue, name="ConsolePrintListener").start()
@@ -19,7 +22,10 @@ class ConsolePrintListener:
         self._listener.subscribe()
         while (not self._should_shut_down):
             if (self._listener.has_next()):
-                print(self._listener.next())
+                entry = self._listener.next()
+                print(entry)
+                with open(self._logfile, "a") as log:
+                    log.writelines(f"{entry}\n")
         print("Closing server console listener")
 
     def stop(self):
@@ -34,7 +40,7 @@ if __name__ == "__main__":
     ip = configs.get("Server", "ip")
 
     manager = ServerManager(server_dir, config_file)
-    listener = ConsolePrintListener(manager)
+    listener = ConsolePrintListener(manager, "log")
     listener.start()
 
     discord_server.prep_client(manager, os.path.join("config", "operators.txt"), os.path.join("config", "owners.txt"), ip)
