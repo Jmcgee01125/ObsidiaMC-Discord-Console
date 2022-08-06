d9787fcd53b8197b4cc475e31d6ee04aee68aca2
Jmcgee1125 <jmcgee01125@gmail.com>
2022-08-06 11:21:27 -0400

    update symbols for mobile viewing

--- bot/helpers/symbols.py
+++ bot/helpers/symbols.py
@@ -13,7 +13,7 @@ X_SIGN
 # https://www.fileformat.info/info/unicode/char/search.htm
 
 EMPTY_SPACE = "\u200b"
-LEFT_ARROW = "\u2b05"
-RIGHT_ARROW = "\u27a1"
-CHECK_SIGN = "\u2714"
-X_SIGN = "\U0001F5D9"
+LEFT_ARROW = "\u2190"  # blue box - \u2B05
+RIGHT_ARROW = "\u2192"  # blue box - \u27A1
+CHECK_SIGN = "\u2714"  # green box - \u2705
+X_SIGN = "\u2613"  # red - \u274C
